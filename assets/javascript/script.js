ec9a6a7d983c9a83758b0178cb3a16b00fe07723
forxtoisate <hwoolford@gmail.com>
2023-10-14 18:06:56 -0500

    added javascript to show prompt 1 when generate password is clicked and show prompt 2 when submit button is clicked

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,17 +1,31 @@
 // Assignment code here
 
+// prompt1 is hidden until generate button is clicked
+const generateBtn = document.querySelector("#generate");
+const getPrompt1 = document.querySelector("#prompt");
+generateBtn.addEventListener("click", () => {
+  getPrompt1.style.display = "block"
+})
+
+// prompt2 is hidden until submit button is clicked
+const submitBtn = document.querySelector("#submit");
+const getPrompt2 = document.querySelector("#prompt2");
+submitBtn.addEventListener("click", () => {
+  getPrompt2.style.display = "block"
+})
+
 
 // Get references to the #generate element
-const generateBtn = document.querySelector("#generate");
+// const generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
-function writePassword() {
-  const password = generatePassword();
-  const passwordText = document.querySelector("#password");
+// function writePassword() {
+//   const password = generatePassword();
+//   const passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+//   passwordText.value = password;
 
-}
+// }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+// generateBtn.addEventListener("click", writePassword);
