0f11638c601d7fbe6acd5722315992a760b90a1d
forxtoisate <hwoolford@gmail.com>
2023-10-18 12:36:47 -0500

    Added the rest of the confirm messages

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,5 +1,5 @@
 // Assignment code here
-const length = ["8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41", "42", "43", "44", "45", "46", "47", "48", "49", "50", "51", "52", "53", "54", "55", "56", "57", "58", "59", "60", "61", "62", "63", "64", "65", "66", "67", "68", "69", "70", "71", "72", "73", "74", "75", "76", "77", "78", "79", "80", "81", "82", "83", "84", "85", "86", "87", "88", "89", "90", "91", "92", "93", "94", "95", "96", "97", "98", "99", "100", "101", "102", "103", "104", "105", "106", "107", "108", "109", "110", "111", "112", "113", "114", "115", "116", "117", "118", "119", "120", "121", "122", "123", "124", "125", "126", "127", "128"];
+const digits = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 
 const lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
@@ -9,14 +9,25 @@ const special = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", "-", ".", "/"
 
 const generateBtn = document.querySelector("#generate");
 
-generateBtn.addEventListener("click", function() {
-const passwordLength = prompt("How many characters do you want in your password? Type a length of at least 8 characters and no more than 128 characters.");
-if (passwordLength < 8 || passwordLength > 128) {
-  alert("Password length must be at least 8 characters and no more than 128 characters." )
-} else {
-  let lowerCase = confirm("Do you want your password to include lowercase characters?");
+function generatePassword() {
+  generateBtn.addEventListener("click", function() {
+    const passwordLength = prompt("How many characters do you want in your password?");
+    if (passwordLength < 8 || passwordLength > 128) {
+      alert("Password length must be at least 8 characters and no more than 128 characters.")
+    } else {
+      const lowerCase = confirm("Click OK if you want your password to include lowercase characters.");
+      const upperCase = confirm("Click OK if you want your password to include uppercase characters.")
+      const numbers = confirm("Click OK if you want your password to include numbers.")
+      const symbols = confirm("Click OK if you want your password to include special characters.")
+    }
+    })
+
+
+
+
+  return "Generated password will go here."
 }
-})
+
 
 
 
@@ -31,3 +42,12 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+
+
+
+
+
+
+// const length = ["8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41", "42", "43", "44", "45", "46", "47", "48", "49", "50", "51", "52", "53", "54", "55", "56", "57", "58", "59", "60", "61", "62", "63", "64", "65", "66", "67", "68", "69", "70", "71", "72", "73", "74", "75", "76", "77", "78", "79", "80", "81", "82", "83", "84", "85", "86", "87", "88", "89", "90", "91", "92", "93", "94", "95", "96", "97", "98", "99", "100", "101", "102", "103", "104", "105", "106", "107", "108", "109", "110", "111", "112", "113", "114", "115", "116", "117", "118", "119", "120", "121", "122", "123", "124", "125", "126", "127", "128"];
