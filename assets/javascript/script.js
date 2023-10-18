ca67ee96e3a01cf5905bb9b6756605422b42b269
forxtoisate <hwoolford@gmail.com>
2023-10-18 17:50:17 -0500

    added verification that at least one character type was selected

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,4 +1,3 @@
-// Assignment code here
 var passwordLength = 8;
 var combinedArrays = []
 
@@ -10,22 +9,39 @@ const upper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", 
 
 const special = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~"];
 
-
 var generateBtn = document.querySelector("#generate");
 generateBtn.addEventListener("click", writePassword);
 
-
-function writePassword() {
-  var allPrompts = prompts();
-  if (allPrompts) {
-    var password = generatePassword();
-    var passwordText = document.querySelector("#password");
-    passwordText.value = password;
-  } else {
-    passwordText.value = "";
+function prompts() {
+  passwordLength = prompt("How many characters do you want in your password?");
+  
+  if (isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) {
+      alert("Please choose a number from 8 to 128 characters long.");
+      return false;
   }
-}
 
+  if (confirm("Click OK if you want your password to include numbers.")) {
+      combinedArrays = combinedArrays.concat(digits);
+  }
+
+  if (confirm("Click OK if you want your password to include lowercase letters.")) {
+      combinedArrays = combinedArrays.concat(lower);
+  }
+
+  if (confirm("Click OK if you want your password to include uppercase letters.")) {
+      combinedArrays = combinedArrays.concat(upper);
+  }
+
+  if (confirm("Click OK if you want your password to include special characters.")) {
+      combinedArrays = combinedArrays.concat(special);
+  }
+
+  if (combinedArrays.length === 0) {
+      alert("Please select at least one criteria for your password.");
+      return false;
+  }
+  return true;
+}
 
 function generatePassword() {
   var password = "";
@@ -36,25 +52,14 @@ function generatePassword() {
   return password;
 }
 
-
-function prompts() {
-  combinedArrays = [];
-  passwordLength = parseInt(prompt("How many characters do you want in your password?"));
-  if (isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) {
-        alert("Please choose a number from 8 to 128 characters long.");
-        return false;
+function writePassword() {
+  var answeredPrompts = prompts();
+  if (answeredPrompts) {
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
+    passwordText.value = password;
+  } else {
+    passwordText.value = "";
   }
-  if (confirm("Click OK if you want your password to include numbers.")) {
-    combinedArrays = combinedArrays.concat(digits);
-  }
-  if (confirm("Click OK if you want your password to include lowercase letters.")) {
-    combinedArrays = combinedArrays.concat(lower);
-  }
-  if (confirm("Click OK if you want your password to include uppercase letters.")) {
-    combinedArrays = combinedArrays.concat(upper);
-  }
-  if (confirm("Click OK if you want your password to include special characters.")) {
-    combinedArrays = combinedArrays.concat(special);
-  }
-    return true;
 }
+
